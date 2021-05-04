81af3857c94da794c402d8127dd2dd41aa1df37d
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-05-04 16:44:18 +0000

    changed => to fucntion

--- src/js/component/board.js
+++ src/js/component/board.js
@@ -4,47 +4,96 @@ import PrintValue from "./printvalue.js";
 
 const Board = () => {
 	const [value, setValue] = useState(true);
-	const changeValue = () => {
-		if (value == true) {
-			setValue(false);
-		}
-		if (value == false) {
-			setValue(true);
-		}
-		return 0;
-	};
+	const [allValues, setAllValues] = useState([
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		""
+	]);
+	//let allValues = ["", "", "", "", "", "", "", "", ""];
+
+	function changeValue(position) {
+		/*if (value == true) {setValue(false);}
+		if (value == false) {setValue(true);}*/
+		setValue(!value);
+		//allValues[position] = value;
+		//setAllValues((allValues[position] = "polla"));
+		console.log(value);
+	}
+
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(0);
+					}}>
 					<PrintValue value={value} />
 				</div>
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(1);
+					}}>
 					<PrintValue value={value} />
 				</div>
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(2);
+					}}>
 					<PrintValue value={value} />
 				</div>
 			</div>
 			<div className="row">
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(3);
+					}}>
 					<PrintValue value={value} />
 				</div>
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(4);
+					}}>
 					<PrintValue value={value} />
 				</div>
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(5);
+					}}>
 					<PrintValue value={value} />
 				</div>
 			</div>
 			<div className="row">
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(6);
+					}}>
 					<PrintValue value={value} />
 				</div>
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(7);
+					}}>
 					<PrintValue value={value} />
 				</div>
-				<div className="col-4 square" onClick={changeValue}>
+				<div
+					className="col-4 square"
+					onClick={() => {
+						changeValue(8);
+					}}>
 					<PrintValue value={value} />
 				</div>
 			</div>
--- src/js/component/printvalue.js
+++ src/js/component/printvalue.js
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 const PrintValue = props => {
 	const [value, setValue] = useState("polla");
 	let print = "";
-	if (value == true) {
+	//let check = 0;
+	if (value == true /*&& check == 0*/) {
 		print = "X";
+		//check = check + 1;
 	}
-	if (value == false) {
+	if (value == false /*&& check == 0*/) {
 		print = "O";
+		//check = check + 1;
 	}
 	return (
 		<div className="value" onClick={() => setValue(props.value)}>
